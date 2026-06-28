9709dc3c454a809e3e184f3a962365e77ff715ca
iamduck67 <garyxu0101@gmail.com>
2026-06-27 22:01:08 -0400

    initial set up

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit_extras.let_it_rain import rain
+
+st.title("This is my blogpost🗿💀💀☠️💀☠️💀🗿💀💀☠️💀💀☠️💀💀💀🗿💀🗿💀🗿💀🗿🗿💀🗿")
+
+st.header("This is the sigma")
+st.subheader("Charlie is not sigma")
+
+st.markdown("""
+"My name is Gary. I am a student that is currently and obligently in the education system's way of comprehending the grade "6". I am also rather fluent in three things that human beings decide to think it is a good idea to call languages. My farvourite word in the currently most updated version of the English dictionary is 'pneumonoultramicroscopicsilicovolcanoconiosis' a 48 letter word that is interesting to say the least. Contrary to popular belief, it is possible to make sentences without the letter a. Here's an example: It is indeed possible to configure sentences while disincluding the first letter of the English Lexicon""")
+
+
+if st.button("send ballons"):
+    st.balloons()
+
+st.image("https://media.tenor.com/USVKtXwWCfsAAAAM/why-cry-why-pepe-why.gif")
+
+if st.button('click me'):
+   rain(
+      emoji='🍿',
+      font_size=50,
+      falling_speed=5,
+      animation_length="10"
+
+   )
